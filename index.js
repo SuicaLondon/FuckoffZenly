b19af9aebba917c5eab7a2fdca67d754efc884c5
SuicaDavid <genglanghuang@gmail.com>
2021-09-17 00:24:26 +0100

    scan the path of each html

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('hello world')
+const fs = require('fs')
+const path = require('path')
+
+
+async function getFolderNames() {
+    return await new Promise((resolve, reject) => {
+        fs.readdir('./', (err, files) => {
+            resolve(files.filter(file => file.includes('Zenly Data')))
+        })
+    })
+}
+
+
+async function directToLocation(names) {
+    return new Promise((resolve, reject)=> {
+        names.forEach(async name => {
+            let locationsPath = path.join(__dirname, name, 'locations')
+            await enterAllLocationFile(locationsPath)
+        })
+    })
+}
+
+async function enterAllLocationFile(locationsPath) {
+    fs.readdir(locationsPath, (err, files) => {
+        let filteredFile = files.filter(file => !Number.isNaN(parseInt(file, 10)))
+        filteredFile.forEach(file=> {
+            let yearPath = path.join(locationsPath, file)
+            fs.readdir(yearPath, (err, files)=> {
+                let filteredFile = files.filter(file => !Number.isNaN(parseInt(file, 10)))
+                filteredFile.forEach(file =>{
+                    let monthPath = path.join(yearPath, file)
+                    fs.readdir(monthPath, (err, files) => {
+                        console.log(files)
+                    })
+                })
+            })
+        })
+    })
+}
+
+!async function(){
+    let folderNames = await getFolderNames()
+    directToLocation(folderNames)
+}()

a7f03a58271ded271c33950181e1139923a76757
SuicaDavid <genglanghuang@gmail.com>
2021-09-17 21:23:16 +0100

    change the code

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const { async } = require('regenerator-runtime')
 const jsdom = require('jsdom')
 const {JSDOM} = jsdom
 
+const locations = []
 
-async function getFolderNames() {
-    return await new Promise((resolve, reject) => {
-        fs.readdir('./', (err, files) => {
-            resolve(files.filter(file => file.includes('Zenly Data')))
+function readdirPromise(dir) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dir, (err, files) => {
+            if(err) reject(err)
+            resolve(files)
         })
     })
+}
+
+async function getFolderNames() {
+    let files = await readdirPromise('./')
+    return files.filter(file => file.includes('Zenly Data'))
 }
 
 
@@ -24,43 +31,45 @@ async function directToLocation(names) {
 }
 
 async function enterAllLocationFile(locationsPath) {
-    fs.readdir(locationsPath, (err, files) => {
-        let filteredFile = files.filter(file => !Number.isNaN(parseInt(file, 10)))
-        filteredFile.forEach(file=> {
-            let yearPath = path.join(locationsPath, file)
-            fs.readdir(yearPath, (err, files)=> {
-                let filteredFile = files.filter(file => !Number.isNaN(parseInt(file, 10)))
-                filteredFile.forEach(file =>{
-                    let monthPath = path.join(yearPath, file)
-                    fs.readdir(monthPath, (err, files) => {
-                        let filteredFile = files.filter(file => !Number.isNaN(parseInt(file, 10)))
-                        filteredFile.forEach(file =>{
-                            loadLocationHTMLFile(monthPath, file)
-                        })
-                    })
-                })
+    let yearsFiles = await readdirPromise(locationsPath)
+    yearsFiles = yearsFiles.filter(file => !Number.isNaN(parseInt(file, 10)))
+    yearsFiles.forEach(async yearFile=>{
+        let yearPath = path.join(locationsPath, yearFile)
+        let monthFiles = await readdirPromise(yearPath)
+        monthFiles = monthFiles.filter(file => !Number.isNaN(parseInt(file, 10)))
+        monthFiles.forEach(async monthFile=>{
+            let monthPath = path.join(yearPath, monthFile)
+            let dayFiles = await readdirPromise(monthPath)
+            dayFiles = dayFiles.filter(file => !Number.isNaN(parseInt(file, 10)))
+            dayFiles.forEach(async dayFile=>{
+                let locationData = await loadLocationHTMLFile(monthPath, dayFile)
+                locations.push(locationData)
+                console.log(locations.length)
             })
         })
     })
 }
 
-async function loadLocationHTMLFile(monthPath, file) {
+function loadLocationHTMLFile(monthPath, file) {
     let filePath = path.join(monthPath, file)
     let date = file.slice(0, -5)
-    fs.readFile(filePath, 'utf-8', (err, data)=> {
-        const dom = new JSDOM(data)
-        let nodeList = dom.window.document.querySelectorAll('tbody tr')
-        nodeList.forEach(tr=>{
-            const locationData = {
-                date: date,
-                time: tr.children[0].textContent,
-                latitude: tr.children[1].textContent,
-                longitude: tr.children[2].textContent,
-                altitude: tr.children[3].textContent,
-                bearing: tr.children[4].textContent,
-                speed: tr.children[5].textContent,
-            }
-            console.log(locationData)
+    return new Promise((resolve, reject)=>{
+        fs.readFile(filePath, 'utf-8', (err, data)=> {
+            if (err) reject(err)
+            const dom = new JSDOM(data)
+            let nodeList = dom.window.document.querySelectorAll('tbody tr')
+            nodeList.forEach(tr=>{
+                const locationData = {
+                    date: date,
+                    time: tr.children[0].textContent,
+                    latitude: tr.children[1].textContent,
+                    longitude: tr.children[2].textContent,
+                    altitude: tr.children[3].textContent,
+                    bearing: tr.children[4].textContent,
+                    speed: tr.children[5].textContent,
+                }
+                resolve(locationData)
+            })
         })
     })
 }
